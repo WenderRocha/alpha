c41929d8f94b81648780e332bd8e2692982d31ac
Wender Rocha <you@example.com>
2022-12-20 16:31:01 -0400

    layout inicial da dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -7,52 +7,48 @@
         </template>
 
         <div class="">
-            <div class="grid md:grid-cols-2 gap-4">
+            <div class="grid md:grid-cols-3 gap-4">
                     <div class="w-full p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
-                        <h4 class="1xl text-xl font-semibold leading-tight">Visão Geral</h4>
+                        <h4 class="1xl text-1xl font-semibold leading-tight">Visão Geral</h4>
 
-                        <div class="mt-5 flex justify-around">
+                        <div class="mt-5 flex justify-between">
                             <div>
-                                <h6>Renda mensal</h6>
+                                <h6 class="text-sm">Renda mensal</h6>
                                 <h6 class="font-bold text-emerald-500">R$ + 2500,00</h6>
                             </div>
                             <div>
-                                <h6>Despesa mensal</h6>
+                                <h6 class="text-sm">Despesa mensal</h6>
                                 <h6 class="font-bold text-red-500">R$ + 2500,00</h6>
                             </div>
-                            <div>
-
-                                <Button variant="primary" class="items-center gap-2 max-w-xs"
-                                        v-slot="{ iconSizeClasses }">
-                                    <PresentationChartLineIcon  class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
-                                    <span>Relatórios</span>
-                                </Button>
-                            </div>
+            
                         </div>
                     </div>
+                    <div class="w-full p-6 overflow-hidden bg-emerald-500 rounded-md shadow-md dark:bg-dark-eval-1">
+                 
+                            <h5 class="mb-2 text-1xl font-bold tracking-tight text-white dark:text-white">Balanço do mês</h5>
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">R$ 1500,00</h5>
+                        
+                    </div>
                     <div class="w-full p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
-                        <h4 class="1xl text-xl font-semibold leading-tight">Acesso Rápido</h4>
+                        <h4 class="1xl text-1xl font-semibold leading-tight">Acesso Rápido</h4>
 
                         <div class="mt-5 flex justify-around">
                             <div>
-                                <Button variant="danger" class="items-center gap-2 max-w-xs"
-                                        v-slot="{ iconSizeClasses }">
-                                    <MinusCircleIcon class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
-                                    <span>Despesa</span>
-                                </Button>
-                            </div>
-                            <div>
-                                <Button variant="success" class="items-center gap-2 max-w-xs"
-                                        v-slot="{ iconSizeClasses }">
+                                <Button iconOnly variant="danger" :size="size" title="Despesa" class="justify-center gap-2">
                                     <PlusCircleIcon class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
-                                    <span>Receita</span>
+                                    <span class="sr-only">Despesa</span>
                                 </Button>
                             </div>
                             <div>
-                                <Button variant="secondary" class="items-center gap-2 max-w-xs"
-                                        v-slot="{ iconSizeClasses }">
-                                    <ArrowCircleRightIcon class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
-                                    <span>Transf</span>
+                                <Button iconOnly variant="success" :size="size" title="Receita" class="justify-center gap-2">
+                                    <PlusCircleIcon class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
+                                    <span class="sr-only">Receita</span>
+                                </Button>
+                            </div>
+                            <div>
+                                <Button iconOnly :variant="variant" :size="size" title="Transferência" class="justify-center gap-2" v-slot="{ iconSizeClasses }">
+                                    <ArrowCircleRightIcon aria-hidden="true" :class="iconSizeClasses" />
+                                    <span class="sr-only">Transferência</span>
                                 </Button>
                             </div>
                         </div>
@@ -60,17 +56,52 @@
                 </div>
         </div>
 
-        <div class="grid md:grid-cols-2 gap-4">
-            <div class="mt-10 w-full p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
-                <apexchart type="area" height="350" :options="chartOptions" :series="series"></apexchart>
-            </div>
-            <div class="mt-10 w-full p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
-                <div class="w-full p-6 overflow-hidden bg-emerald-500 rounded-md shadow-md dark:bg-dark-eval-1">
-                    <h5 class="mb-2 text-1xl font-bold tracking-tight text-white dark:text-white">Balanço do mês</h5>
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">R$ 1500,00</h5>
+        <div class="grid md:grid-cols-2 gap-4 mt-10">
+            <div class="d-flex item-center p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
+                <div>
+                    <h5 class="text-sm">Despesa por categorias</h5>
                 </div>
-                <apexchart class="mt-5" type="bar" height="350" :options="balanceChartOptions" :series="balanceSeries"></apexchart>
+                <apexchart  type="pie" :options="CategoriesChartOptions" :series="categoryChartSeries"></apexchart>
             </div>
+            <div class="w-full p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
+                <div>
+                    <h5 class="text-sm">Balanço mensal</h5>
+                </div>
+                <apexchart type="bar" height="350" :options="balanceChartOptions" :series="balanceSeries"></apexchart>
+            </div>
+        </div>
+
+        
+        <div class="grid md:grid-cols-2 gap-4 mt-10">
+            <div class="w-full p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
+                <div class="">
+                    <h5 class="text-sm flex items-center">
+                        <CalendarIcon class="text-gray-600 flex-shrink-0 w-6 h-6 mr-3" aria-hidden="true" />
+                        Contas a receber
+                    </h5>
+                </div>
+
+                <div class="mt-10">
+                    <Alert type="success" class="mb-2">
+                        No momento, não existem contas a receber.
+                    </Alert>
+                </div>
+            </div>
+            <div class="d-flex item-center p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
+                <div class="">
+                    <h5 class="text-sm flex items-center">
+                        <CalendarIcon class="text-gray-600 flex-shrink-0 w-6 h-6 mr-3" aria-hidden="true" />
+                        Contas a pagar
+                    </h5>
+                </div>
+
+                <div class="mt-10">
+                    <Alert type="danger" class="mb-2">
+                        No momento, não existem contas a pagar.
+                    </Alert>
+                </div>
+            </div>
+      
         </div>
 
 
@@ -80,11 +111,11 @@
 
 <script setup>
 import AuthenticatedLayout from '@/Layouts/Authenticated.vue'
-import {TheCard, Modal, Input, Progress, Badge} from 'flowbite-vue'
+import {TheCard, Modal, Input, Progress, Badge, Alert} from 'flowbite-vue'
 import Button from '@/Components/Button.vue'
 import {computed, onBeforeUnmount, onMounted, reactive, ref, watch} from 'vue'
 import {useForm} from "@inertiajs/inertia-vue3";
-import { MinusCircleIcon, PlusCircleIcon, ArrowCircleRightIcon, PresentationChartLineIcon     } from "@heroicons/vue/outline";
+import { MinusCircleIcon, PlusCircleIcon, ArrowCircleRightIcon, PresentationChartLineIcon, CalendarIcon      } from "@heroicons/vue/outline";
 
 import {
     handleScroll,
@@ -184,6 +215,15 @@ const balanceSeries = [
         data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 150]
     }
 ]
+
+const CategoriesChartOptions = reactive({
+    labels: ['categoria 1', 'categoria 2'],
+    legend: {
+        show: false
+    }
+});
+
+const categoryChartSeries = [75, 25, 45, 56, 78]
 
 </script>
 
